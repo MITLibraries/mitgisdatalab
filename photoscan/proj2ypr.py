588dfe90885b15984fc876089965923791ac7acb
jesusg <jesusg@mit.edu>
2019-05-22 10:35:24 -0400

    Use pandas dataframes to save data obtained from photoscan to a csv file

--- photoscan/proj2ypr.py
+++ photoscan/proj2ypr.py
@@ -1,38 +1,67 @@
+import pandas as pd
 import PhotoScan
 
+
 PROJ_PATH = 'E:/UserFiles/jesusg/testing/lowRes/lowScan.psx'
+OUTPUT_PATH = 'E:/UserFiles/jesusg/testing/lowRes/low_data.csv'
 
-# Create a PhotoScan document object that will be used for particular project
-doc = PhotoScan.app.document
 
-# Assign a project to the PhotoScan document object
-doc.open(PROJ_PATH)
+def main():
+    # Create a PhotoScan document object that will be used for particular project
+    doc = PhotoScan.app.document
 
-chunk = PhotoScan.app.document.chunk
+    # Assign a project to the PhotoScan document object
+    doc.open(PROJ_PATH)
 
-print('\nWorking with the following cameras:\n', chunk.cameras, end='\n\n')
+    chunk = PhotoScan.app.document.chunk
 
-first_cam = chunk.cameras[0].photo.meta
-print('Each camera contains the following meta data:\n',
-      first_cam.keys(), end='\n\n')
+    extract_ypr(chunk)
 
-# Iterate through each photo
-for camera in chunk.cameras:
-    # Access specific photo meta data to save yaw, pitch, and roll
-    yaw = float(camera.photo.meta['DJI/GimbalYawDegree'])
-    pitch = float(camera.photo.meta['DJI/GimbalPitchDegree'])
-    roll = float(camera.photo.meta['DJI/GimbalRollDegree'])
+
+# Given a chunk, yaw, pitch, and roll are found and saved as a csv file
+def extract_ypr(chunk):
+    image_list = list()
+    yaw_list = list()
+    pitch_list = list()
+    roll_list = list()
+
+    # Iterate through each photo
+    for camera in chunk.cameras:
+        # Save camera name for reference
+        image_list.append(camera.label)
+
+        # Access specific photo meta data to save yaw, pitch, and roll
+        yaw = float(camera.photo.meta['DJI/GimbalYawDegree'])
+        pitch = float(camera.photo.meta['DJI/GimbalPitchDegree'])
+        roll = float(camera.photo.meta['DJI/GimbalRollDegree'])
     
-    # Save a vector containing yaw, pitch, and roll
-    camera.reference.rotation = PhotoScan.Vector([yaw, pitch, roll])
+        # Save yaw, pitch, and roll
+        yaw_list.append(yaw)
+        pitch_list.append(pitch)
+        roll_list.append(row)
 
-    # Access Absolute and Relative altitudes
+    # Dictionary is created using the data
+    d = {'image_name': image_list, 'yaw': yaw_list,
+        'pitch': pitch_list, 'roll': roll_list}
+
+    # Dictionary is used to create the pandas dataframe
+    df = pd.DataFrame(data=d)
+    
+    # Newly created dataframe is exported to a csv file
+    df.to_csv(OUTPUT_PATH)
+
+
+# Given a camera's metadata the altitudes are examined
+def analyze_altitudes(camera):
+    # Access Absolute, Relative, and GPS altitudes
     abs_alt = float(camera.photo.meta['DJI/AbsoluteAltitude'])
     rel_alt = float(camera.photo.meta['DJI/RelativeAltitude'])
+    gps_alt = float(camera.photo.meta['Exif/GPSAltitude'])
     
-    print(camera.label)
-    print('ref_rot: ', camera.reference.rotation)
-    print('absolute: %f \t relative: %f' % (abs_alt, rel_alt))
+    print(camera.label, 'has the following altitudes:')
+    print('Absolute: %f\n Relative: %f' % (abs_alt, rel_alt))
     
     # GPS altitude is a bit more exact than the absolute altitude
-    #print('GPS altitude: %f' % float(camera.photo.meta['Exif/GPSAltitude']))
+    print('GPS altitude: %f' % gps_alt)
+
+
