876ab519e0aadda247271658b904d4629a47e4b1
Jesus Garcia <jesusjoagar@gmail.com>
2019-07-02 17:27:56 -0400

    Modified versions of files to work on lab super computers.

---
+++ final_process/extract_metadata.py
@@ -0,0 +1,145 @@
+import os 
+import exifread
+import pandas as pd
+from config import * 
+
+
+def main():
+    path = 'E:/UserFiles/jesusg/testing/SpoonOnlyImages'
+    
+    image_paths = find_image_paths(path)
+
+    # Initialize lists needed for each relevant metadata field
+    image_list, yaw_list, pitch_list, roll_list = ([] for i in range(4))
+    yaw_flist, pitch_flist, roll_flist = ([] for i in range(3))
+    abs_list, rel_list = ([] for i in range(2))
+
+    for path in image_paths:
+        # Relevent xmp data is returned
+        rel_xmp = find_xmp_metadata(path)
+    
+        # Prepare values to be transformed to CSV
+        image_list.append(path[-12:]); yaw_list.append(rel_xmp['yaw']);
+        pitch_list.append(rel_xmp['pitch']); roll_list.append(rel_xmp['roll']);
+        yaw_flist.append(rel_xmp['yawf']); 
+        pitch_flist.append(rel_xmp['pitchf']);
+        roll_flist.append(rel_xmp['rollf']);
+        abs_list.append(rel_xmp['abs_alt']);
+        rel_list.append(rel_xmp['rel_alt']);
+
+        real_exif = find_exif_metadata(path)
+        print(real_exif)
+        #print('Altitude:', real_exif['GPS GPSAltitude'])
+        #print('AltitudeRef:', real_exif['GPS GPSAltitudeRef'])
+        #print('Subject Distance range:', real_exif['EXIF SubjectDistanceRange'])
+
+
+    d = {'image_name': image_list, 'yaw': yaw_list,
+        'pitch': pitch_list, 'roll': roll_list,
+        'yaw_flight': yaw_flist, 'pitch_flight': pitch_flist,
+        'roll_flight': roll_flist,
+        'absolute_altitude': abs_list, 'relative_altitude': rel_list}
+
+    df = pd.DataFrame(data=d)
+    print(df.head())
+    
+    df.to_csv(get_full_path('cam_meta_extract.csv'), index=False)
+
+
+def find_image_paths(path):
+    images = list()
+
+    print('About to begin searching. . .')
+    for r, d, f in os.walk(path):
+        print(r, d, f)
+        for file in f:
+            if '.JPG' in file:
+                images.append(os.path.join(r, file))
+
+    return images
+
+
+def find_xmp_metadata(image_path):
+    # Open the file (JPG)
+    image = open(image_path, 'rb')
+    data = image.read()
+
+    # Locate the metadata in the file
+    xmp_start = data.find(b'<x:xmpmeta')
+    xmp_end = data.find(b'</x:xmpmeta')
+    xmp_str = data[xmp_start:xmp_end+12]
+
+    image.close()
+
+    return parse_xmp(xmp_str)
+
+
+def parse_xmp(xmp_str):
+    # Make return xmp string a list to iterate through fields
+    xmp_list = xmp_str.split()
+
+    # Dict to store the needed metadata
+    real_meta = dict()
+
+    for x in xmp_list:
+        name_utf = x.decode('utf-8')
+        if name_utf[:9] == 'drone-dji':
+            # Get the key-value pair for the specific field
+            key, value = parse_xmpfield(name_utf)
+
+            real_meta[key] = value
+
+    #print('xmp_meta_extracted:\n', real_meta)
+    # Dictionary created to store and return relevata xmp metadata
+    rel_xmp = dict()
+
+    rel_xmp['yaw'] = real_meta['drone-dji:GimbalYawDegree']
+    rel_xmp['pitch'] = real_meta['drone-dji:GimbalPitchDegree']
+    rel_xmp['roll'] = real_meta['drone-dji:GimbalRollDegree']
+    rel_xmp['yawf'] = real_meta['drone-dji:FlightYawDegree']
+    rel_xmp['pitchf'] = real_meta['drone-dji:FlightPitchDegree']
+    rel_xmp['rollf'] = real_meta['drone-dji:FlightRollDegree']
+    rel_xmp['abs_alt'] = real_meta['drone-dji:AbsoluteAltitude']
+    rel_xmp['rel_alt'] = real_meta['drone-dji:RelativeAltitude']
+    xmp_list = real_meta.keys()
+
+    return rel_xmp
+
+
+def find_exif_metadata(image_path):
+    f = open(image_path, 'rb')
+
+    tags = exifread.process_file(f)
+
+    exif_dict = dict()
+
+    for tag in tags.keys():
+        if tag in GOOD_META:
+            exif_dict[tag] = tags[tag]
+
+    f.close()
+
+    return exif_dict
+
+
+def parse_xmpfield(field):
+    field_elems = field.split('=')
+    key = field_elems[0]
+    value = convert_value(field_elems[1])
+
+    return key, value
+
+
+def convert_value(raw_value):
+    init_chars = raw_value[:2]
+
+    if init_chars not in ['"+', '"-']:
+        return raw_value
+    elif init_chars == '"+':
+        return float(raw_value[2:-1])
+    else:
+        return -float(raw_value[2:-1])
+
+
+if __name__ == '__main__':
+    main()
---
+++ final_process/llh_extraction.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import os
+import arcpy
+from arcpy.sa import ExtractValuesToPoints
+from arcpy import env
+from config import *
+
+env.overwriteOutput = True
+
+def main():
+    # Look at Digital Elevation Model and Flight Point layer to get flyingheight
+    arcpy.CheckOutExtension('Spatial')
+
+    ExtractValuesToPoints(PTS_SHP, DSM_FLIGHT, FLIGHT_PTS_SHP, 'NONE', 'VALUE_ONLY')
+
+    
+    try:
+        # Need to do a little math to find actual flying height, also get altitude
+        # NOTE
+        with arcpy.da.SearchCursor(FLIGHT_PTS_SHP, ("ImageName", "Latitude", "Longitude", "Altitude", "RASTERVALU")) as cursor:
+            image_list = list()
+            lat_list = list()
+            long_list = list()
+            flyingHeig_list = list()
+            for row in cursor:
+                image_list.append(row[0])
+                lat_list.append(row[1])
+                long_list.append(row[2])
+                flyingHeig_list.append(row[3] - row[4])
+
+            # Data is saved as a dictionary
+            d = {'image_name': image_list, 'latitude': lat_list, 
+                'longitude': long_list, 'flying_height': flyingHeig_list}
+
+            # Dictionary is transformed into a pandas dataframe
+            df = pd.DataFrame(data=d)
+            df.head()
+
+            # Export data as a csv
+            df.to_csv(ARCMAP_CSV, index=False)
+
+            del cursor
+    except:
+        print(arcpy.GetMessages())
+        raise
+
+
+if __name__ == '__main__':
+    main()
